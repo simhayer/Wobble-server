f25a0cfb39fc5ed5773e8f1cc8435a00ddff3578
Simrat Hayer <shayer@magnetforensics.com>
2024-07-18 23:14:26 -0400

    added turn config

--- config.js
+++ config.js
@@ -1,18 +1,46 @@
+// const configurationPeerConnection = {
+//     iceServers: [{
+//         urls: "stun:stun.stunprotocol.org"
+//     }]
+// }
+
 const configurationPeerConnection = {
-    iceServers: [{
-        urls: "stun:stun.stunprotocol.org"
-    }]
-}
+  iceServers: [
+    {
+      urls: "stun:stun.relay.metered.ca:80",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:80?transport=tcp",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+    {
+      urls: "turn:global.relay.metered.ca:443",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+    {
+      urls: "turns:global.relay.metered.ca:443?transport=tcp",
+      username: "aca60fb4568ea274f8245009",
+      credential: "Zi/jzkiJuI2fmwLx",
+    },
+  ],
+};
 
 const offerSdpConstraints = {
-    "mandatory": {
-        "OfferToReceiveAudio": true,
-        "OfferToReceiveVideo": true,
-    },
-    "optional": [],
-}
+  mandatory: {
+    OfferToReceiveAudio: true,
+    OfferToReceiveVideo: true,
+  },
+  optional: [],
+};
 
 module.exports = {
-    configurationPeerConnection,
-    offerSdpConstraints
-}
+  configurationPeerConnection,
+  offerSdpConstraints,
+};
