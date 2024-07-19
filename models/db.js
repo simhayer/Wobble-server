490a9244ed2f644484ffc95756fb004f6f9d8586
Simrat Hayer <shayer@magnetforensics.com>
2024-07-18 22:06:02 -0400

    added mongo url

--- models/db.js
+++ models/db.js
@@ -1,23 +1,26 @@
 // db.js
-const mongoose = require('mongoose');
-const AppData = require('./AppData'); // Ensure the path is correct
+const mongoose = require("mongoose");
+const AppData = require("./AppData"); // Ensure the path is correct
 
-const localDB = `mongodb://0.0.0.0:27017/role_auth`;
+//const localDB = `mongodb://0.0.0.0:27017/role_auth`;
+
+const localDB =
+  "mongodb+srv://hayersimrat23:Messi999@cluster0.ad77vid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const initializeAppData = async () => {
   try {
     const highestUserID = await AppData.findOne();
     if (!highestUserID) {
-      await AppData.create({highestUserID: 1});
+      await AppData.create({ highestUserID: 1 });
     }
   } catch (error) {
-    console.error('Error initializing app data:', error);
+    console.error("Error initializing app data:", error);
   }
 };
 
 const connectDB = async () => {
   await mongoose.connect(localDB, {});
-  console.log('MongoDB Connected');
+  console.log("MongoDB Connected");
   await initializeAppData();
 };
 
